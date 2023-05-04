cb9662b8010e9dcd4bf38391e718419da49923ea
Dave Wilding <tech@dpw.me>
2023-05-04 03:31:21 +0000

    Refactored functionality into SDK

--- sdk.js
+++ sdk.js
@@ -1,11 +1,10 @@
 import * as openpgp from "https://unpkg.com/openpgp@5.x/dist/openpgp.min.mjs";
 
-export const publicInbox = {};
-// export const inboxOwner = {};
-
-export function setNewUser() {
-  // TODO
-}
+export const publicInbox = {
+  userID: null,
+  pgpPublicKey: null,
+  pgpPublicKeyObj: null
+};
 
 export async function setPublicInbox(userID) {
   let response;
@@ -32,9 +31,9 @@ export async function setPublicInbox(userID) {
 
 export async function sendMessage(message) {
   if (
-    message == ""
-    || publicInbox.userID === undefined
-    || publicInbox.pgpPublicKeyObj === undefined) {
+    publicInbox.userID === null
+    || publicInbox.pgpPublicKeyObj === null
+  ) {
     return false;
   }
   const encryptedMessage = await openpgp.encrypt({
@@ -66,10 +65,73 @@ export async function sendMessage(message) {
   return result.success;
 }
 
-export function setInboxOwner(apiKey, pgpPrivateKey) {
-  // TODO
+export const inboxOwner = {
+  apiKey: null,
+  pgpPrivateKey: null,
+  pgpPrivateKeyObj: null
+};
+
+export async function setInboxOwner(apiKey, pgpPrivateKey) {
+  inboxOwner.apiKey = apiKey;
+  inboxOwner.pgpPrivateKey = pgpPrivateKey;
+  inboxOwner.pgpPrivateKeyObj = await openpgp.readKey({
+    armoredKey: inboxOwner.pgpPrivateKey
+  });
 }
 
-export function fetchMessages() {
-  // TODO
+export async function fetchMessages() {
+  if (
+    publicInbox.userID === null
+    || inboxOwner.apiKey === null
+    || inboxOwner.pgpPrivateKeyObj === null
+  ) {
+    return [];
+  }
+  let response;
+  try {
+    response = await fetch("https://api.frogtab.com/post-remove-messages", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        user_id: publicInbox.userID,
+        api_key: inboxOwner.apiKey
+      })
+    });
+  }
+  catch (error) {
+    return [];
+  }
+  if (!response.ok) {
+    return [];
+  }
+  const messages = [];
+  const encryptedMessages = await response.json();
+  for (const encryptedMessage of encryptedMessages) {
+    const decrypted = await openpgp.decrypt({
+      message: await openpgp.readMessage({
+        armoredMessage: encryptedMessage
+      }),
+      decryptionKeys: inboxOwner.pgpPrivateKeyObj
+    });
+    messages.push(decrypted.data);
+  }
+  return messages;
+}
+
+export async function generateLocalCredentials() {
+  const userID = crypto.randomUUID();
+  const apiKey = crypto.randomUUID();
+  const pgpKey = await openpgp.generateKey({
+    userIDs: [{
+      email: `user_${userID}@frogtab.com`
+    }]
+  });
+  return {
+    userID: userID,
+    apiKey: apiKey,
+    pgpPublicKey: pgpKey.publicKey,
+    pgpPrivateKey: pgpKey.privateKey
+  };
 }
