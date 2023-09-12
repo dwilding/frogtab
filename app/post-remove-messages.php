a576316a3fa3945a9c94f55da93626643a34e435
Dave Wilding <tech@dpw.me>
2023-09-13 06:39:56 +0800

    Authenticate user via the database

--- app/post-remove-messages.php
+++ app/post-remove-messages.php
@@ -31,27 +31,27 @@ if (!array_key_exists('api_key', $request_body)) {
 }
 $api_key = $request_body['api_key'];
 
-// Load config
-$users = json_decode(file_get_contents($_SERVER['APP_DIR_CONFIG'] . '/users.json'), true);
+// Connect to database
+$db = new PDO('sqlite:' . $_SERVER['APP_DIR_DATA'] . '/sqlite.db');
 
-// Verify user ID
-if (!array_key_exists($user_id, $users)) {
-  respond_with_failure();
-}
-
-// Verify API key
-if ($users[$user_id]['api_key'] != $api_key) {
+// Try to select user
+$select_user = $db->prepare('SELECT user_id, api_key FROM users WHERE user_id = :user_id AND api_key = :api_key');
+$select_user->bindParam(':user_id', $user_id);
+$select_user->bindParam(':api_key', $api_key);
+$select_user->execute();
+$select_user_result = $select_user->fetch(PDO::FETCH_ASSOC);
+if (!$select_user_result) {
   respond_with_failure();
 }
 
 // Remove messages
 $messages = [];
 $db = new PDO('sqlite:' . $_SERVER['APP_DIR_DATA'] . '/sqlite.db');
-$sql_select = $db->prepare('SELECT * FROM messages WHERE for = :for ORDER BY id ASC');
-$sql_select->bindParam(':for', $user_id);
-$sql_select->execute();
-$results = $sql_select->fetchAll(PDO::FETCH_ASSOC);
-foreach ($results as $result) {
+$select_messages = $db->prepare('SELECT * FROM messages WHERE for = :for ORDER BY id ASC');
+$select_messages->bindParam(':for', $user_id);
+$select_messages->execute();
+$select_messages_result = $select_messages->fetchAll(PDO::FETCH_ASSOC);
+foreach ($select_messages_result as $result) {
   $id = $result['id'];
   $message = $result['message'];
   array_push($messages, $message);
