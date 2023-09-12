95c7bd25abfb7dd593ea8bd69e614444f1af6c41
Dave Wilding <tech@dpw.me>
2023-09-13 06:32:52 +0800

    Get user from database

--- app/open/get-user.php
+++ app/open/get-user.php
@@ -15,11 +15,15 @@ if (empty($_GET['user_id'])) {
 }
 $user_id = $_GET['user_id'];
 
-// Load config
-$users = json_decode(file_get_contents($_SERVER['APP_DIR_CONFIG'] . '/users.json'), true);
+// Connect to database
+$db = new PDO('sqlite:' . $_SERVER['APP_DIR_DATA'] . '/sqlite.db');
 
-// Verify user ID
-if (!array_key_exists($user_id, $users)) {
+// Try to select user
+$select_user = $db->prepare('SELECT user_id, pgp_public_key FROM users WHERE user_id = :user_id');
+$select_user->bindParam(':user_id', $user_id);
+$select_user->execute();
+$select_user_result = $select_user->fetch(PDO::FETCH_ASSOC);
+if (!$select_user_result) {
   respond_with_failure();
 }
 
@@ -28,7 +32,7 @@ echo json_encode([
   'success' => true,
   'user' => [
     'user_id' => $user_id,
-    'pgp_public_key' => $users[$user_id]['pgp_public_key']
+    'pgp_public_key' => $select_user_result['pgp_public_key']
   ]
 ]);
 
