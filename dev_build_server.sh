73bf26eb02fe7e4f057dd141103748cf9fc06925
Dave Wilding <tech@dpw.me>
2024-07-04 10:12:38 +0800

    Store database in parent dir of repo

--- dev_build_server.sh
+++ dev_build_server.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-DIR_CALLER="$(pwd)"
-
-cd "$(dirname "$0")" # cd to repo root
 cp -r app/* server/public
 DIR_PACKAGES="$(pwd)/server/packages"
+DIR_PARENT="$(cd .. && pwd)"
 
 cd server/public
 sed -i'.backup' 's/data-server-base=\"https:\/\/frogtab.com\"/data-server-base=\"\"/' index.html icon-*.html help.html
@@ -12,7 +10,7 @@ sed -i'.backup' 's/data-location=\"local\"/data-location=\"server\"/' send.html
 rm *.backup
 cat <<EOF > .htaccess
 SetEnv DIR_PACKAGES "$DIR_PACKAGES"
-SetEnv FILE_SQLITEDB "$DIR_CALLER/frogtab.db"
+SetEnv FILE_SQLITEDB "$DIR_PARENT/frogtab.db"
 RewriteEngine On
 RewriteCond %{REQUEST_FILENAME} !-f
 RewriteCond %{REQUEST_FILENAME}.php -f
@@ -24,4 +22,4 @@ EOF
 cd ../packages
 composer install
 
-cd "$DIR_CALLER"
+cd ../..
