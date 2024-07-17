1789df4fbbc6b87f84de6defa2882f93b3e40dba
Dave Wilding <tech@dpw.me>
2024-07-17 12:11:49 +0800

    Link to Linux snap from help page (server only)

--- dev_build_server.sh
+++ dev_build_server.sh
@@ -7,6 +7,7 @@ DIR_PARENT="$(cd .. && pwd)"
 cd server/public
 sed -i'.backup' 's/data-server-base=\"https:\/\/frogtab.com\/\"/data-server-base=\"\"/' index.html icon-*.html help.html
 sed -i'.backup' 's/data-location=\"local\"/data-location=\"server\"/' send.html
+sed -i'.backup' 's/\(<a href="https:\/\/github.com\/dwilding\/frogtab"\)/<a href="https:\/\/snapcraft.io\/frogtab" target="_blank">Linux snap<\/a> • \1/' help.html
 rm *.backup
 cat <<EOF > .htaccess
 SetEnv DIR_PACKAGES "$DIR_PACKAGES"
