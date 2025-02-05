bb1f380c11c35c59592369aee40e8eaef8c6c1a6
Dave Wilding <tech@dpw.me>
2025-02-05 15:25:39 +0800

    Added pairing to the server

--- local/frogctl/local_server/server.py
+++ local/frogctl/local_server/server.py
@@ -1,18 +1,26 @@
 import sys
 import os
 import signal
+import json
 import logging
 import flask
 
-from helpers import Config, backend
+def read_json(json_file: str) -> dict:
+    with open(json_file, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+def write_json(data: dict, json_file: str) -> None:
+    with open(json_file, 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
 
 
-# Load config
+# Load config (including running state)
 
 args = sys.argv[1:]
 if len(args) != 1:
     sys.exit(2)
-config = Config(args[0])
+config_file = args[0]
+config = read_json(config_file)
 
 
 # Create a Flask app
@@ -24,19 +32,19 @@ app = flask.Flask(__name__, static_url_path='/')
 
 @app.route('/')
 def serve_index():
-    return flask.render_template('index.html', server_base=config.registration_server)
+    return flask.render_template('index.html', server_base=config['registrationServer'])
 
 @app.route('/icon-normal')
 def serve_icon_normal():
-    return flask.render_template('icon-normal.html', server_base=config.registration_server)
+    return flask.render_template('icon-normal.html', server_base=config['registrationServer'])
 
 @app.route('/icon-notify')
 def serve_icon_notify():
-    return flask.render_template('icon-notify.html', server_base=config.registration_server)
+    return flask.render_template('icon-notify.html', server_base=config['registrationServer'])
 
 @app.route('/help')
 def serve_help():
-    return flask.render_template('help.html', server_base=config.registration_server)
+    return flask.render_template('help.html', server_base=config['registrationServer'])
 
 @app.route('/<string:file>')
 def serve_file(file):
@@ -46,34 +54,66 @@ def serve_file(file):
         return flask.send_from_directory(app.static_folder, f'{file}.html')
 
 
-# Define service routes
+# Define service routes for the web app
 
-@app.route('/service/get-methods')
-def get_methods():
-    return backend.methods()
+@app.route('/service/post-pair', methods=['POST'])
+def post_pair():
+    body = flask.request.get_json()
+    success = False
+    if body['force'] or not config['internalState']['pairingKey']:
+        config['internalState']['pairingKey'] = body['key']
+        write_json(config, config_file)
+        success = True
+    elif body['key'] == config['internalState']['pairingKey']:
+        success = True
+    return {
+        'success': success
+    }
 
 @app.route('/service/post-data', methods=['POST'])
 def post_data():
     body = flask.request.get_json()
-    return backend.save_data(body['key'], body['data'])
+    if body['key'] != config['internalState']['pairingKey']:
+        return {
+            'success': False
+        }
+    write_json(body['data'], config['backupFile'])
+    return {
+        'success': True
+    }
+
+@app.route('/service/post-remove-messages', methods=['POST'])
+def remove_messages():
+    body = flask.request.get_json()
+    if body['key'] != config['internalState']['pairingKey']:
+        return {
+            'success': False
+        }
+    messages = config['internalState']['messages']
+    config['internalState']['messages'] = []
+    write_json(config, config_file)
+    return {
+        'success': True,
+        'messages': messages
+    }
+
+
+# Define service routes for the controller
+
+@app.route('/service/get-running')
+def get_running():
+    return flask.make_response('', 204)
 
 @app.route('/service/post-add-message', methods=['POST'])
 def add_message():
     body = flask.request.get_json()
-    backend.add_message(body['message'])
+    config['internalState']['messages'].insert(0, body['message'])
+    write_json(config, config_file)
     return flask.make_response('', 204)
-
-@app.route('/service/post-remove-messages', methods=['POST'])
-def remove_messages():
-    return backend.remove_messages()
 
 @app.route('/service/post-stop', methods=['POST'])
 def post_stop():
     os.kill(os.getpid(), signal.SIGINT)
-    return flask.make_response('', 204)
-
-@app.route('/service/get-running')
-def get_running():
     return flask.make_response('', 204)
 
 
@@ -88,4 +128,4 @@ def add_custom_header(response):
 # Run Flask
 
 logging.getLogger('werkzeug').disabled = True
-app.run(port=config.port)
+app.run(port=config['port'])
