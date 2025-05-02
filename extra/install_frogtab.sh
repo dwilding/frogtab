7a44df9f5710a31201bffee8bdd0c6b2f4ebd92f
Dave Wilding <tech@dpw.me>
2025-05-02 17:13:10 +0800

    add extra script to install the server

---
+++ extra/install_frogtab.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+shopt -s dotglob
+
+protected="$PWD"
+public="$1"
+if [ -z "$public" ]; then
+  echo "Error: no public directory specified"
+  exit 2
+fi
+if [ ! -d "$public" ]; then
+  echo "Error: directory '$public' does not exist"
+  exit 1
+fi
+if [ ! "$2" = "--refresh" ] && [ -n "$(find "$public" -mindepth 1 -print -quit)" ]; then
+  echo "Error: directory '$public' is not empty"
+  exit 1
+fi
+if [ "$2" = "--refresh" ]; then
+  rm -rf frogtab
+fi
+if [ -d "frogtab" ]; then
+  echo "Error: directory '$protected/frogtab' already exists"
+  exit 1
+fi
+wget -O frogtab.zip "https://github.com/dwilding/frogtab/archive/refs/heads/server-install.zip"
+unzip frogtab.zip -d frogtab
+rm frogtab.zip
+
+cd "$protected/frogtab/frogtab-server-install"
+scripts/build_server.sh "$protected/frogtab.db"
+if [ "$2" = "--refresh" ]; then
+  rm -rf "$public"/*
+fi
+mv server/public/* "$public"
