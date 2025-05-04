31c175561bca575467e73dcceeee42a253bbb9c0
Dave Wilding <tech@dpw.me>
2025-05-04 15:05:08 +0800

    release: add script for automated server install (#21)

---
+++ extra/frogtab.com/private/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+extra="https://raw.githubusercontent.com/dwilding/frogtab/refs/heads/main/extra"
+
+cd /home/protected
+wget -O install_frogtab.sh "$extra/install_frogtab.sh"
+chmod +x install_frogtab.sh
+./install_frogtab.sh /home/public --overwrite
+
+cd /home/public
+wget -qO- "$extra/frogtab.com/public/.htaccess" >> .htaccess
+wget -O sitemap.xml "$extra/frogtab.com/public/sitemap.xml"
+sed -i'.backup' 's/data-registration=\"short\"/data-registration=\"long\"/' help.html
+rm *.backup
---
+++ extra/install_frogtab.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+shopt -s dotglob  # Bash-specific option
+
+set -e
+
+print_help() {
+  echo -e "\033[0;35m$1\033[0m"
+}
+
+print_error() {
+  echo -e "\033[1;31mError:\033[0m $1"
+}
+
+require_user_approval() {
+  echo "Do you want to continue? (y/n)"
+  read -r response
+  if [ "$response" != "y" ] && [ "$response" != "Y" ]; then
+    exit 1
+  fi
+}
+
+protected="$PWD"  # Non-public directory for the source and database
+
+# Check which public directory we should use
+public="$1"
+if [ -z "$public" ]; then
+  print_error "no public directory specified"
+  exit 2
+fi
+if [ ! -d "$public" ]; then
+  print_error "directory '$public' does not exist"
+  exit 1
+fi
+if [ ! "$2" = "--overwrite" ] && [ -n "$(find -L "$public" -mindepth 1 -print -quit)" ]; then
+  echo "Public directory '$public' is not empty"
+  require_user_approval
+fi
+
+# Fetch the source
+if [ ! "$2" = "--overwrite" ] && [ -d "frogtab" ]; then
+  echo "Source code directory '$protected/frogtab' already exists"
+  require_user_approval
+fi
+print_help "Downloading source code…"
+wget -O frogtab.zip "https://github.com/dwilding/frogtab/archive/refs/heads/main.zip"
+rm -rf frogtab
+unzip frogtab.zip -d frogtab
+rm frogtab.zip
+
+# Build the server and prepare the public directory
+print_help "Installing server…"
+cd "$protected/frogtab"
+mv frogtab-main/app frogtab-main/server .
+frogtab-main/scripts/build_server.sh "$protected/frogtab.db"
+rm -rf frogtab-main
+rm -rf app
+rm -rf "$public"/*
+mv server/public/* "$public"
+print_help "Done!"
